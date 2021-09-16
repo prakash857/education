04dfc9d7114693382ee5deac3c10b5b7f99830d9
prakash857 <prakashsiva857@gmail.com>
2021-09-16 21:46:46 +0530

    educationdetails

---
+++ src/org/edu/Arts.java
@@ -0,0 +1,42 @@
+package org.edu;
+
+public class Arts extends Education {
+	private void bSc() {
+		System.out.println("bsc in maths");
+	}
+	private void bEd() {
+		System.out.println("B.ed in English");
+	}
+	private void bA() {
+		System.out.println("BA in science");
+
+	}
+	private void bBA() {
+		System.out.println("BBA in Tamil");
+
+	}
+	@Override
+	public void ug(String dept) {
+		System.out.println("ug in "+dept);
+	}
+	@Override
+	public void pg(String dept) {
+		System.out.println("pg in "+dept);
+	}
+	public static void main(String[] args) {
+		Arts a = new Arts();
+		a.bEd();
+		a.bA();
+		a.bBA();
+		a.bSc();
+		a.ug("CSE");
+		a.pg("Thermal");
+		
+		
+	}
+	
+	
+	
+	
+
+}
---
+++ src/org/edu/Education.java
@@ -0,0 +1,12 @@
+package org.edu;
+
+public class Education {
+	public void ug(String dept) {
+		System.out.println("ug in "+dept);
+
+	}
+	public void pg(String dept) {
+		System.out.println("pg in "+dept );
+
+	}
+}
